260b2c7bab9430dbdb9e08aa7c4e65ad405730ea
Arturs Are <arearturs@gmail.com>
2022-01-04 11:41:02 +0200

    Fix API loading

--- Project/API/RatesAPI.swift
+++ Project/API/RatesAPI.swift
@@ -11,15 +11,14 @@ import Networking
 
 struct RatesAPI: NetworkingService {
     private static let apiKey = "5d568348421e7b08a00fbd6a06333fbb"
+    let keyParams = ["access_key" : apiKey]
 
     let network: NetworkingClient = {
         var client = NetworkingClient(baseURL: "http://api.exchangeratesapi.io/v1")
-        client.headers["access_key"] = apiKey
-        client.parameterEncoding = .json
         return client
     }()
 
     func getRates() -> AnyPublisher<BaseRate, Error> {
-        return get("latest")
+        return get("/latest", params: keyParams)
     }
 }
